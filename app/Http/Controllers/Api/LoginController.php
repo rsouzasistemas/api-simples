85b8f77f237ec514929185b2e006cd5cef4331dd
Rodrigo de Souza <rs_manutencao@hotmail.com>
2023-06-21 18:34:54 -0300

    Incluído README

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller
 
     public function logout()
     {
-        // auth()->user()->currentAccessToken()->delete(); //Deleta apenas o token atual do usuário
         auth()->user()->tokens()->delete(); //Deleta todos os tokens do usuário
 
         return response()->json([], 204);
